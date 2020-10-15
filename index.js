6203ff0c049a32a4e8d9202ca74d60a2777d6e99
diegolopez03 <diego232.lopez@gmail.com>
2020-10-14 23:33:39 -0600

    Crear ruta

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 // Importar express
 const express = require("express");
 
+// Importar handlebars como template engine
+const exphbs = require("express-handlebars");
+
+
 // Crear un servidor express
 const app = express();
+
+// Indicar a express utilizar handlebars como template engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+
+app.set("view engine", "hbs");
+
+// Crear una ruta para /
+// Información sobre los verbos HTTP
+// https://developer.mozilla.org/es/docs/Web/HTTP/Methods
+app.get("/", (req, res, next) => {
+    res.render("ejercicio7");
+  });
+
 
 // Inicializar el servidor en un puerto en específico
 app.listen(5000, () => {
